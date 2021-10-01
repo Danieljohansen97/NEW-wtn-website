f837d482d5c9e573f00bb15f6b315857450942a8
Danieljohansen97 <danieljohansen1997@hotmail.com>
2021-10-01 09:10:22 +0200

    Add HomePage.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
+import HomePage from "./components/homePage/HomePage";
 
 function App() {
   return (
     <div className="App">
+      <HomePage />
       <Button variant="contained">Hello</Button>
       <Button variant="contained">Hello</Button>
+      <p>hei</p>
     </div>
   );
 }
