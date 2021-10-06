24f343f8a258823e6bcb5059c19b62f45ecd8015
Danieljohansen97 <danieljohansen1997@hotmail.com>
2021-10-06 06:21:34 +0200

    Add a figure to About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,25 @@
-import React from 'react'
+// General React imports
+import React from "react";
+// Bootstrap imports
+import Figure from "react-bootstrap/Figure";
+import Container from "react-bootstrap/Container";
 
 const About = () => {
-    return (
-        <div>
-            <h3>About</h3>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Container>
+        <Figure className="float-start">
+          <Figure.Image
+            width={250}
+            height={260}
+            alt="The founder of Taekkyeon"
+            src="https://via.placeholder.com/171x180"
+          />
+          <Figure.Caption>Grunnleggeren av taekkyeon her.</Figure.Caption>
+        </Figure>
+      </Container>
+    </div>
+  );
+};
 
-export default About
+export default About;
